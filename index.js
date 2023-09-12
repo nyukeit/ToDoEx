e633d54659f1b213071b678d5f6cfc7c7d13170e
Nyukeit <nyukeit@outlook.com>
2023-09-12 15:10:39 +0200

    [Trials](Add) Delete button

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
+// Import Modules
 import express from 'express';
 import bodyParser from 'body-parser';
 
+// Declare server
 const app = express();
 const port = 3000;
 
+// Declare module use
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// App logic variables
 const toDoList = [];
 const toDoWork = [];
 
-
-// POST route for the homepage
+// Add task route for Today page
 app.post("/addtask", (req, res) => {
-  const newItemToday = req.body["list-item"]
-  toDoList.push(newItemToday);
+  if (req.body["list-item"] !== "") {
+    toDoList.push(req.body["list-item"]);
+  }
   res.redirect("/");
 });
 
-// Get the rendered homepage with task list
+app.post('/', (req,res) => {
+
+  // DELETEbtn
+  if(req.body["delete"]) {
+  
+    const index = items.indexOf(req.body.delete); 
+    if (index > -1) { // only splice array when item is found
+        items.splice(index, 1);
+    }
+  }
+//items is my array where I'm storing the newly added to-do list items.
+  res.redirect('/');
+});
+
+// Render Today page with tasks added
 app.get("/", (req, res) => {
   const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const currentDay = dayNames[new Date().getDay()];
   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const currentMonth = monthNames[new Date().getMonth()];
-  res.render("index.ejs", { today: currentDay, month: currentMonth, toDoList: toDoList });
+  const toDoListCount = toDoList.length;
+  res.render("index.ejs", { today: currentDay, month: currentMonth, toDoList: toDoList, toDoListCount: toDoListCount });
 });
 
-// POST route for the Work List
+// Add task route for Work page
 app.post("/addtaskwork", (req, res) => {
-  const newItemWork = req.body["list-item-work"]
-  toDoWork.push(newItemWork);
+  if (req.body["list-item-work"] !== "") {
+    toDoWork.push(req.body["list-item-work"]);
+  }
   res.redirect("/work");
 });
 
-// Get the rendered work list page with task list
+// Render Work page with tasks added
 app.get("/work", (req, res) => {
-  res.render("work.ejs", { toDoWork: toDoWork });
+  const toDoWorkCount = toDoWork.length;
+  res.render("work.ejs", { toDoWork: toDoWork, toDoWorkCount: toDoWorkCount });
 });
 
+// Open port for app
 app.listen(port, () => {
   console.log(`Server running on ${port}.`);
 });
